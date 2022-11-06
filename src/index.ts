9e6a0766551235c93b7571397ee521e9566f310c
syhzrl <syhzrl@gmail.com>
2022-11-06 22:53:36 +0800

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,83 @@
 import express from "express";
-// import http from 'http';
-// import mongoose from "mongoose";
+import http from 'http';
+import mongoose from "mongoose";
 
-// import { config } from './config/config';
+import { config } from './config/config';
 
-// import Logging from "./lib/Logging";
+import Logging from "./lib/Logging";
 
-// import cardRoutes from './routes/Card';
+import cardRoutes from './routes/Card';
 
-// const router = express();
+const router = express();
 
-// // Connect to Mongo
-// mongoose.connect(config.mongo.url, { retryWrites: true, w: 'majority' })
-//     .then(() => {
-//         Logging.info('Connected to mongoDB');
-//         startServer();
-//     })
-//     .catch((error) => {
-//         Logging.error('Unable to connect: ');
-//         Logging.error(error);
-//     });
+// Connect to Mongo
+mongoose.connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        Logging.info('Connected to mongoDB');
+        startServer();
+    })
+    .catch((error) => {
+        Logging.error('Unable to connect: ');
+        Logging.error(error);
+    });
 
 
-// // Only start server if Mongo Connect
-// const startServer = () => {
-//     router.use((req, res, next) => {
-//         // Log the Request
-//         Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+// Only start server if Mongo Connect
+const startServer = () => {
+    router.use((req, res, next) => {
+        // Log the Request
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
 
-//         res.on('finish', () => {
-//             Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
-//         });
+        res.on('finish', () => {
+            Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
 
-//         next();
-//     });
+        next();
+    });
 
-//     router.use(express.urlencoded({ extended: true }));
-//     router.use(express.json());
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
 
-//     // Rules of our API
-//     router.use((req, res, next) => {
-//         res.header('Access-Control-Allow-Origin', '*');
-//         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    // Rules of our API
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-//         if (req.method == 'OPTIONS') {
-//             res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//             return res.status(200).json({});
-//         }
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
 
-//         next();
-//     });
+        next();
+    });
 
-//     // Routes
-//     router.use('/cards', cardRoutes);
+    // Routes
+    router.use('/cards', cardRoutes);
 
-//     // Healthcheck
-//     router.get('/ping', (req, res, next) => {
-//         res.status(200).json({ message: 'pong' });
-//     });
+    // Healthcheck
+    router.get('/ping', (req, res, next) => {
+        res.status(200).json({ message: 'pong' });
+    });
 
-//     // Error Handling
-//     router.use((req, res, next) => {
-//         const error = new Error('not found');
-//         Logging.error(error);
+    // Error Handling
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
 
-//         return res.status(404).json({ message: error.message });
-//     });
+        return res.status(404).json({ message: error.message });
+    });
 
-//     http.createServer(router).listen(config.server.port, () => {
-//         Logging.info(`Server is running on port ${config.server.port}.`);
-//     });
-// }
+    http.createServer(router).listen(config.server.port, () => {
+        Logging.info(`Server is running on port ${config.server.port}.`);
+    });
+}
 
 // export = router;
 
-const app = express();
+// const app = express();
 
-app.get('/ping', (req, res, next) => {
-    res.status(200).json({ message: 'pong' });
-});
+// app.get('/ping', (req, res, next) => {
+//     res.status(200).json({ message: 'pong' });
+// });
 
-app.listen(9090, () => console.log('it works'));
+// app.listen(9090, () => console.log('it works'));
